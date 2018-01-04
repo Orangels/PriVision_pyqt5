33f1eb94fa19db07e64bffe6021284c78e9e6374
Yang <1013608889@qq.com>
2018-01-04 19:02:51 +0800

    Update mainwindow_privision.py

--- mainwindow_privision.py
+++ mainwindow_privision.py
@@ -35,7 +35,12 @@ from libs.toolbar import *
 from libs.canvas import *
 from libs.zoomwidget import *
 from libs.cameradevice import *
+from libs.utils import *
+
+from modules.poseestimator import *
 from modules.facedetector import *
+from modules.objdetector import *
+from modules.trafficdetector import *
 from pypriv.tools.visualize import *
 from pypriv import variable as V
 
@@ -61,7 +66,7 @@ task4_out = Queue(2)
 
 
 def Task1_process(msg_in, msg_out):
-    Worker1 = FaceDetector(gpu_id=1)
+    Worker1 = PoseEstimator()
     while True:
         if not msg_in.empty():
             frame = msg_in.get()
@@ -70,7 +75,7 @@ def Task1_process(msg_in, msg_out):
 
 
 def Task2_process(msg_in, msg_out):
-    Worker2 = GetImage()
+    Worker2 = FaceDetector(gpu_id=1)
     while True:
         if not msg_in.empty():
             frame = msg_in.get()
@@ -79,8 +84,7 @@ def Task2_process(msg_in, msg_out):
 
 
 def Task3_process(msg_in, msg_out):
-    Worker3 = GetImage(gpu_id=2)
-    # Worker3 = FaceAlign(gpu_id=2)
+    Worker3 = ObjDetector(gpu_id=2)
     while True:
         if not msg_in.empty():
             frame = msg_in.get()
@@ -89,7 +93,8 @@ def Task3_process(msg_in, msg_out):
 
 
 def Task4_process(msg_in, msg_out):
-    Worker4 = GetImage(gpu_id=3)
+    Worker4 = TrafficDetector(gpu_id=3)
+    # Worker4 = FaceAlign(gpu_id=2)
     while True:
         if not msg_in.empty():
             frame = msg_in.get()
@@ -122,9 +127,9 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         # canvas main window
-        self.setWindowIcon(QIcon('./files/pose_icon.png'))
+        self.setWindowIcon(QIcon(cfg.ICONS.LOGO))
         self.canvas = Canvas()
-        self.canvas.image = QImage("./files/backl.jpg")
+        self.canvas.image = QImage(cfg.ICONS.BACKGROUND)
         self.canvas.zoomRequest.connect(self.zoomRequest)
 
         scroll = QScrollArea()
@@ -142,8 +147,7 @@ class MainWindow(QMainWindow):
         zoom.setDefaultWidget(self.zoomWidget)
         self.zoomWidget.setWhatsThis(
             u"Zoom in or out of the image. Also accessible with"
-            " %s and %s from the canvas." % (fmtShortcut("Ctrl+[-+]"),
-                                             fmtShortcut("Ctrl+Wheel")))
+            " %s and %s from the canvas." % (fmtShortcut("Ctrl+[-+]"), fmtShortcut("Ctrl+Wheel")))
         self.zoomWidget.setEnabled(False)
 
         self.scalers = {
@@ -159,7 +163,7 @@ class MainWindow(QMainWindow):
         self.zoomWidget.valueChanged.connect(self.paintCanvas)
         self.resized.connect(self.adjustScale)
         # camera
-        self.camera_device = CameraDevice(video_path='./files/none.avi')
+        self.camera_device = CameraDevice(video_path=cfg.GLOBAL.VIDEO1)
         self.camera_device.newFrame.connect(self.onNewImage)
         self.camera_device.video_time_out.connect(self.clear)
 
@@ -182,50 +186,50 @@ class MainWindow(QMainWindow):
         self.ui.model5_bn.clicked.connect(self.apply_model5)
 
         # top button functions
-        self.ui.thread1_bn.setIcon(QIcon("./files/icons/icon-top/realtime-mode-bright.png"))
-        self.ui.thread1_bn.setIconSize(QSize(110, 20))
-        self.ui.thread2_bn.setIcon(QIcon("./files/icons/icon-top/playback1-bright.png"))
-        self.ui.thread2_bn.setIconSize(QSize(110, 20))
-        self.ui.thread3_bn.setIcon(QIcon("./files/icons/icon-top/playback2-bright.png"))
-        self.ui.thread3_bn.setIconSize(QSize(110, 20))
-        self.ui.thread4_bn.setIcon(QIcon("./files/icons/icon-top/picture-mode-bright.png"))
-        self.ui.thread4_bn.setIconSize(QSize(110, 20))
-        self.ui.thread5_bn.setIcon(QIcon("./files/icons/icon-top/loading-mode-bright.png"))
-        self.ui.thread5_bn.setIconSize(QSize(110, 20))
+        self.ui.thread1_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT1))
+        self.ui.thread1_bn.setIconSize(QSize(cfg.ICONS.TOP_SIZE[0], cfg.ICONS.TOP_SIZE[1]))
+        self.ui.thread2_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT2))
+        self.ui.thread2_bn.setIconSize(QSize(cfg.ICONS.TOP_SIZE[0], cfg.ICONS.TOP_SIZE[1]))
+        self.ui.thread3_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT3))
+        self.ui.thread3_bn.setIconSize(QSize(cfg.ICONS.TOP_SIZE[0], cfg.ICONS.TOP_SIZE[1]))
+        self.ui.thread4_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT4))
+        self.ui.thread4_bn.setIconSize(QSize(cfg.ICONS.TOP_SIZE[0], cfg.ICONS.TOP_SIZE[1]))
+        self.ui.thread5_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT5))
+        self.ui.thread5_bn.setIconSize(QSize(cfg.ICONS.TOP_SIZE[0], cfg.ICONS.TOP_SIZE[1]))
         # left button functions
-        self.ui.play_bn.setIcon(QIcon("./files/icons/icon-left/play-bright.png"))
-        self.ui.play_bn.setIconSize(QSize(110, 70))
-        self.ui.pause_bn.setIcon(QIcon("./files/icons/icon-left/pause-bright.png"))
-        self.ui.pause_bn.setIconSize(QSize(110, 70))
-        self.ui.record_bn.setIcon(QIcon("./files/icons/icon-left/record-bright.png"))
-        self.ui.record_bn.setIconSize(QSize(110, 70))
-        self.ui.empty_bn.setIcon(QIcon("./files/icons/icon-left/empty-bright.png"))
-        self.ui.empty_bn.setIconSize(QSize(110, 70))
-        self.ui.setting_bn.setIcon(QIcon("./files/icons/icon-left/setting-bright.png"))
-        self.ui.setting_bn.setIconSize(QSize(110, 70))
-        self.ui.exit_bn.setIcon(QIcon("./files/icons/icon-left/exit.png"))
-        self.ui.exit_bn.setIconSize(QSize(110, 70))
+        self.ui.play_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP1))
+        self.ui.play_bn.setIconSize(QSize(cfg.ICONS.LEFT_SIZE[0], cfg.ICONS.LEFT_SIZE[1]))
+        self.ui.pause_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP2))
+        self.ui.pause_bn.setIconSize(QSize(cfg.ICONS.LEFT_SIZE[0], cfg.ICONS.LEFT_SIZE[1]))
+        self.ui.record_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP3))
+        self.ui.record_bn.setIconSize(QSize(cfg.ICONS.LEFT_SIZE[0], cfg.ICONS.LEFT_SIZE[1]))
+        self.ui.empty_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP4))
+        self.ui.empty_bn.setIconSize(QSize(cfg.ICONS.LEFT_SIZE[0], cfg.ICONS.LEFT_SIZE[1]))
+        self.ui.setting_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP5))
+        self.ui.setting_bn.setIconSize(QSize(cfg.ICONS.LEFT_SIZE[0], cfg.ICONS.LEFT_SIZE[1]))
+        self.ui.exit_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP6))
+        self.ui.exit_bn.setIconSize(QSize(cfg.ICONS.LEFT_SIZE[0], cfg.ICONS.LEFT_SIZE[1]))
         # right button icons
-        self.ui.model1_bn.setIcon(QIcon("./files/icons/icon-right/human-detection-bright.png"))
-        self.ui.model1_bn.setIconSize(QSize(110, 70))
-        self.ui.model2_bn.setIcon(QIcon("./files/icons/icon-right/human-pose-bright.png"))
-        self.ui.model2_bn.setIconSize(QSize(110, 70))
-        self.ui.model3_bn.setIcon(QIcon("./files/icons/icon-right/face-align-bright.png"))
-        self.ui.model3_bn.setIconSize(QSize(110, 70))
-        self.ui.model4_bn.setIcon(QIcon("./files/icons/icon-right/face-recog-bright.png"))
-        self.ui.model4_bn.setIconSize(QSize(110, 70))
-        self.ui.model5_bn.setIcon(QIcon("./files/icons/icon-right/human-seg-bright.png"))
-        self.ui.model5_bn.setIconSize(QSize(110, 70))
+        self.ui.model1_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP1))
+        self.ui.model1_bn.setIconSize(QSize(cfg.ICONS.RIGHT_SIZE[0], cfg.ICONS.RIGHT_SIZE[1]))
+        self.ui.model2_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP2))
+        self.ui.model2_bn.setIconSize(QSize(cfg.ICONS.RIGHT_SIZE[0], cfg.ICONS.RIGHT_SIZE[1]))
+        self.ui.model3_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP3))
+        self.ui.model3_bn.setIconSize(QSize(cfg.ICONS.RIGHT_SIZE[0], cfg.ICONS.RIGHT_SIZE[1]))
+        self.ui.model4_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP4))
+        self.ui.model4_bn.setIconSize(QSize(cfg.ICONS.RIGHT_SIZE[0], cfg.ICONS.RIGHT_SIZE[1]))
+        self.ui.model5_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP5))
+        self.ui.model5_bn.setIconSize(QSize(cfg.ICONS.RIGHT_SIZE[0], cfg.ICONS.RIGHT_SIZE[1]))
 
         # task special param
         self.image_render = GetImage()
         self.flag_savevideo = False
-        self.info_header = u"Hello! Welcome from PriVision!\n  "
+        self.info_header = u"Hello from PriVision!\n  "
         self.shown_info(self.info_header)
         self.allframes = []
         self.video_writer = None
         self.savevideo_counting = 0
-        self.savevideo_max = 1800 * 20
+        self.savevideo_max = cfg.GLOBAL.SAVE_VIDEO_MAX_SECOND
 
     def resizeEvent(self, event):
         self.resized.emit()
@@ -292,11 +296,11 @@ class MainWindow(QMainWindow):
 
     def start_cap(self):
         # self.ui.play_bn.setIcon(QIcon("./files/icons/icon-left/play-color.png"))
-        self.ui.pause_bn.setIcon(QIcon("./files/icons/icon-left/pause-bright.png"))
+        self.ui.pause_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP2))
         self.camera_device.paused = False
 
     def stop_cap(self):
-        self.ui.pause_bn.setIcon(QIcon("./files/icons/icon-left/pause-color.png"))
+        self.ui.pause_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP2.replace('bright', 'color')))
         self.camera_device.paused = True
 
     def save_video(self):
@@ -305,15 +309,16 @@ class MainWindow(QMainWindow):
             self.savevideo_counting = 0
             video_name = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
             fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-            self.video_writer = cv2.VideoWriter('/home/user/Desktop/{}_save.avi'.format(video_name), fourcc, 20,
-                                                (960, 540))
-            self.ui.record_bn.setIcon(QIcon("./files/icons/icon-left/record-color.png"))
+            self.video_writer = cv2.VideoWriter('{}/{}_save.avi'.format(cfg.GLOBAL.SAVE_VIDEO_PATH, video_name), fourcc,
+                                                cfg.GLOBAL.SAVE_VIDEO_FPS,
+                                                cfg.GLOBAL.SAVE_VIDEO_SIZE)
+            self.ui.record_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP3.replace('bright', 'color')))
         else:
             self.video_writer.release()
             self.savevideo_counting = 0
             self.flag_savevideo = False
             self.video_writer = None
-            self.ui.record_bn.setIcon(QIcon("./files/icons/icon-left/record-bright.png"))
+            self.ui.record_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP3))
         pass
 
     def clear(self):
@@ -325,82 +330,81 @@ class MainWindow(QMainWindow):
     def apply_model1(self):
         if cfg.GLOBAL.F_MODEL1:
             cfg.GLOBAL.F_MODEL1 = False
-            self.ui.model1_bn.setIcon(QIcon("./files/icons/icon-right/human-detection-bright.png"))
+            self.ui.model1_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP1))
         else:
             cfg.GLOBAL.F_MODEL1 = True
-            self.ui.model1_bn.setIcon(QIcon("./files/icons/icon-right/human-detection-color.png"))
+            self.ui.model1_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP1.replace('bright', 'color')))
 
     def apply_model2(self):
         if cfg.GLOBAL.F_MODEL2:
             cfg.GLOBAL.F_MODEL2 = False
-            self.ui.model2_bn.setIcon(QIcon("./files/icons/icon-right/human-pose-bright.png"))
+            self.ui.model2_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP2))
         else:
             cfg.GLOBAL.F_MODEL2 = True
-            self.ui.model2_bn.setIcon(QIcon("./files/icons/icon-right/human-pose-color.png"))
+            self.ui.model2_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP2.replace('bright', 'color')))
 
     def apply_model3(self):
         if cfg.GLOBAL.F_MODEL3:
             cfg.GLOBAL.F_MODEL3 = False
-            self.ui.model3_bn.setIcon(QIcon("./files/icons/icon-right/face-align-bright.png"))
+            self.ui.model3_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP3))
         else:
             cfg.GLOBAL.F_MODEL3 = True
-            self.ui.model3_bn.setIcon(QIcon("./files/icons/icon-right/face-align-color.png"))
+            self.ui.model3_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP3.replace('bright', 'color')))
 
     def apply_model4(self):
         if cfg.GLOBAL.F_MODEL4:
             cfg.GLOBAL.F_MODEL4 = False
-            self.ui.model4_bn.setIcon(QIcon("./files/icons/icon-right/face-recog-bright.png"))
+            self.ui.model4_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP4))
         else:
             cfg.GLOBAL.F_MODEL4 = True
-            self.ui.model4_bn.setIcon(QIcon("./files/icons/icon-right/face-recog-color.png"))
+            self.ui.model4_bn.setIcon(QIcon(cfg.ICONS.RIGHT_TOP4.replace('bright', 'color')))
 
     def apply_model5(self):
         pass
 
     def start_camera(self):
-        self.ui.thread1_bn.setIcon(QIcon("./files/icons/icon-top/realtime-mode-color.png"))
-        self.ui.thread2_bn.setIcon(QIcon("./files/icons/icon-top/playback1-bright.png"))
-        self.ui.thread3_bn.setIcon(QIcon("./files/icons/icon-top/playback2-bright.png"))
-        self.ui.thread4_bn.setIcon(QIcon("./files/icons/icon-top/picture-mode-bright.png"))
-        self.ui.thread5_bn.setIcon(QIcon("./files/icons/icon-top/loading-mode-bright.png"))
+        self.ui.thread1_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT1.replace('bright', 'color')))
+        self.ui.thread2_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT2))
+        self.ui.thread3_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT3))
+        self.ui.thread4_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT4))
+        self.ui.thread5_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT5))
         self.camera_device = CameraDevice()
         self.camera_device.newFrame.connect(self.onNewImage)
         self.clear()
 
     def start_video1(self):
-        self.ui.thread1_bn.setIcon(QIcon("./files/icons/icon-top/realtime-mode-bright.png"))
-        self.ui.thread2_bn.setIcon(QIcon("./files/icons/icon-top/playback1-color.png"))
-        self.ui.thread3_bn.setIcon(QIcon("./files/icons/icon-top/playback2-bright.png"))
-        self.ui.thread4_bn.setIcon(QIcon("./files/icons/icon-top/picture-mode-bright.png"))
-        self.ui.thread5_bn.setIcon(QIcon("./files/icons/icon-top/loading-mode-bright.png"))
-        self.camera_device.set_video_path('./files/news.avi')
+        self.ui.thread1_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT1))
+        self.ui.thread2_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT2.replace('bright', 'color')))
+        self.ui.thread3_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT3))
+        self.ui.thread4_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT4))
+        self.ui.thread5_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT5))
+        self.camera_device.set_video_path(cfg.GLOBAL.VIDEO1)
         self.clear()
 
     def start_video2(self):
-        self.ui.thread1_bn.setIcon(QIcon("./files/icons/icon-top/realtime-mode-bright.png"))
-        self.ui.thread2_bn.setIcon(QIcon("./files/icons/icon-top/playback1-bright.png"))
-        self.ui.thread3_bn.setIcon(QIcon("./files/icons/icon-top/playback2-color.png"))
-        self.ui.thread4_bn.setIcon(QIcon("./files/icons/icon-top/picture-mode-bright.png"))
-        self.ui.thread5_bn.setIcon(QIcon("./files/icons/icon-top/loading-mode-bright.png"))
-        self.camera_device.set_video_path('./files/shanxi_traffic/0500320.avi')
+        self.ui.thread1_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT1))
+        self.ui.thread2_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT2))
+        self.ui.thread3_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT3.replace('bright', 'color')))
+        self.ui.thread4_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT4))
+        self.ui.thread5_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT5))
+        self.camera_device.set_video_path(cfg.GLOBAL.VIDEO2)
         self.clear()
 
     def start_pic(self):
-        self.ui.thread1_bn.setIcon(QIcon("./files/icons/icon-top/realtime-mode-bright.png"))
-        self.ui.thread2_bn.setIcon(QIcon("./files/icons/icon-top/playback1-bright.png"))
-        self.ui.thread3_bn.setIcon(QIcon("./files/icons/icon-top/playback2-bright.png"))
-        self.ui.thread4_bn.setIcon(QIcon("./files/icons/icon-top/picture-mode-color.png"))
-        self.ui.thread5_bn.setIcon(QIcon("./files/icons/icon-top/loading-mode-bright.png"))
-        self.camera_device.set_video_path('./files/xx2.avi')
+        self.ui.thread1_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT1))
+        self.ui.thread2_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT2))
+        self.ui.thread3_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT3))
+        self.ui.thread4_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT4.replace('bright', 'color')))
+        self.ui.thread5_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT5))
+        self.camera_device.set_video_path(cfg.GLOBAL.VIDEO3)
         self.clear()
 
     def load_video(self):
-        self.ui.thread1_bn.setIcon(QIcon("./files/icons/icon-top/realtime-mode-bright.png"))
-        self.ui.thread2_bn.setIcon(QIcon("./files/icons/icon-top/playback1-bright.png"))
-        self.ui.thread3_bn.setIcon(QIcon("./files/icons/icon-top/playback2-bright.png"))
-        self.ui.thread4_bn.setIcon(QIcon("./files/icons/icon-top/picture-mode-bright.png"))
-        self.ui.thread5_bn.setIcon(QIcon("./files/icons/icon-top/loading-mode-color.png"))
-        self.camera_device.set_video_path('./files/facedet.avi')
+        self.ui.thread1_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT1))
+        self.ui.thread2_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT2))
+        self.ui.thread3_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT3))
+        self.ui.thread4_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT4))
+        self.ui.thread5_bn.setIcon(QIcon(cfg.ICONS.TOP_LEFT5.replace('bright', 'color')))
         self.clear()
 
     @QtCore.pyqtSlot(np.ndarray)
@@ -408,7 +412,7 @@ class MainWindow(QMainWindow):
         self.adjustScale()
 
         frame = np.asarray(frame[:, :])
-        frame = cv2.resize(frame, (960, 640))
+        frame = cv2.resize(frame, cfg.GLOBAL.IM_SHOW_SIZE)
         self.allframes.append(frame)
         vis = frame.copy()
 
@@ -459,27 +463,25 @@ class MainWindow(QMainWindow):
                     result4 = None
                 break
         if result1 is not None:
-            vis = draw_fancybbox(vis, result1)
+            vis = draw_pose_kpts(vis, result1, V.COLORMAP19, V.POSE19_LINKPAIR)
         if result2 is not None:
-            if cfg.GLOBAL.F_MODEL3:
-                vis = draw_pose_kpts(vis, result2, V.COLORMAP19, V.POSE19_LINKPAIR)
-            else:
-                vis = draw_pose_kpts(vis, result2, V.COLORMAP19, V.POSE19_LINKPAIR)
+            vis = draw_fancybbox(vis, result2)
         if result3 is not None:
             vis = draw_fancybbox(vis, result3)
-            # vis = draw_face68_kpts(vis, result3)
         if result4 is not None:
-            vis = draw_fancybbox(vis, result4, attri=True)
+            vis = draw_fancybbox(vis, result4)
+            # vis = draw_fancybbox(vis, result4, attri=True)
+            # vis = draw_face68_kpts(vis, result4)
         dt = clock() - t
 
         if self.flag_savevideo and self.savevideo_counting <= self.savevideo_max:
-            save_im = cv2.resize(vis, (960, 540))
+            save_im = cv2.resize(vis, cfg.GLOBAL.SAVE_VIDEO_SIZE)
             self.video_writer.write(save_im)
             self.savevideo_counting += 1
         elif self.savevideo_counting > self.savevideo_max:
             self.savevideo_counting = 0
             self.flag_savevideo = False
-            self.ui.record_bn.setIcon(QIcon("./files/icons/icon-left/record-bright.png"))
+            self.ui.record_bn.setIcon(QIcon(cfg.ICONS.LEFT_TOP3))
         draw_str(vis, (30, 30), 'speed: %.1f fps' % (min(1.0 / dt, 30)))
 
         if len(self.allframes) % 15 == 0:
@@ -491,7 +493,7 @@ class MainWindow(QMainWindow):
             cur_info += u'--------------------\n  '
             self.shown_info(cur_info)
 
-        vis = cv2.resize(vis, (960, 640))
+        vis = cv2.resize(vis, cfg.GLOBAL.IM_SHOW_SIZE)
         image = QImage(vis.tostring(), vis.shape[1], vis.shape[0], QImage.Format_RGB888).rgbSwapped()
 
         self.canvas.update_image(image)
